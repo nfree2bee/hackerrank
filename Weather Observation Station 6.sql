e46cbf4c8a0003b2879672e3dd0ba9c0163c6981
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-07-18 16:30:44 +0700

    Create Weather Observation Station 6.sql

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,24 @@
+--Problem Statement
+
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+*/
+
+--Solution
+
+SELECT DISTINCT Country FROM Customers WHERE Country REGEXP "^[GF]" 
+
+or
+
+SELECT DISTINCT Country FROM Customers WHERE Country RLIKE "^[GF]"
