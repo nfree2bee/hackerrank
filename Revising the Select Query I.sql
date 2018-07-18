d71cf9ada36b7fc23b077086421488766f216433
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-07-18 13:58:33 +0700

    Update Revising the Select Query I.sql

--- Revising the Select Query I.sql
+++ Revising the Select Query I.sql
@@ -1,17 +1,18 @@
 --Problem Statement
 /*
 Given a table CITY that holds data for five fields namely ID, NAME, COUNTRY CODE, DISTRICT and POPULATION.
-+-------------+------------+
-| Field       |   Type     |
-+-------------+------------+
-| ID          | NUMBER     |
-| NAME        | VARCHAR(17)|
-| COUNTRY CODE| VARCHAR(3) |
-| DISTRICT    | VARCHAR(20)|
-| POPULATION  | NUMBER     |
-+-------------+------------+
+
++-------------+-------------+
+| Field       |   Type      |
++-------------+-------------+
+| ID          | NUMBER      |
+| NAME        | VARCHAR2(17)|
+| COUNTRY CODE| VARCHAR2(3) |
+| DISTRICT    | VARCHAR2(20)|
+| POPULATION  | NUMBER      |
++-------------+-------------+
  
-Write a query Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
+Write a query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
 */
 
 --Solution
