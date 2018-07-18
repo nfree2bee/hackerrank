78a2731ac970088d5ac50f1be441a37b104130fb
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-07-18 14:00:00 +0700

    Create Select All.sql

---
+++ Select All.sql
@@ -0,0 +1,20 @@
+--Problem Statement
+/*
+Given a table CITY that holds data for five fields namely ID, NAME, COUNTRY CODE, DISTRICT and POPULATION.
+
++-------------+-------------+
+| Field       |   Type      |
++-------------+-------------+
+| ID          | NUMBER      |
+| NAME        | VARCHAR2(17)|
+| COUNTRY CODE| VARCHAR2(3) |
+| DISTRICT    | VARCHAR2(20)|
+| POPULATION  | NUMBER      |
++-------------+-------------+
+ 
+Write query all columns (attributes) for every row in the CITY table. 
+*/
+
+--Solution
+
+SELECT * FROM CITY
