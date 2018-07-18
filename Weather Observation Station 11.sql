ea110931b7dfd1b80c5fc8565a7babf51d8e13cc
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-07-18 21:35:06 +0700

    Create Weather Observation Station 11.sql

---
+++ Weather Observation Station 11.sql
@@ -0,0 +1,20 @@
+--Problem Statement
+
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+*/
+
+--Solution
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP "^[^aiueo]" OR CITY REGEXP "[^aiueo]$";
